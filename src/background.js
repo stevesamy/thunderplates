22245c7aba45d240dd96968be0450570d1d7f75d
stevesamy <oyster16@gmail.com>
2025-06-27 16:52:50 -0600

    Add context menu to insert templates

--- src/background.js
+++ src/background.js
@@ -1,6 +1,47 @@
 // background.js
 
-browser.runtime.onInstalled.addListener(() => {
+async function createContextMenus() {
+  await browser.menus.removeAll();
+  const { templates = [] } = await browser.storage.local.get('templates');
+  if (templates.length === 0) {
+    return;
+  }
+  const parentId = browser.menus.create({
+    id: 'thunderplates-parent',
+    title: 'Insert template',
+    contexts: ['editable'],
+  });
+  for (const [index, t] of templates.entries()) {
+    browser.menus.create({
+      id: `thunderplates-${index}`,
+      parentId,
+      title: t.name,
+      contexts: ['editable'],
+    });
+  }
+}
+
+browser.runtime.onInstalled.addListener(async () => {
   // Initialize storage with empty templates array if not present
-  browser.storage.local.get({ templates: [] }).then(() => {});
+  await browser.storage.local.get({ templates: [] });
+  createContextMenus();
+});
+
+browser.runtime.onStartup.addListener(createContextMenus);
+
+browser.storage.onChanged.addListener((changes, area) => {
+  if (area === 'local' && changes.templates) {
+    createContextMenus();
+  }
+});
+
+browser.menus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId.startsWith('thunderplates-')) {
+    const index = parseInt(info.menuItemId.split('-')[1], 10);
+    const { templates = [] } = await browser.storage.local.get('templates');
+    const template = templates[index];
+    if (template) {
+      await browser.compose.insertHTML(tab.id, template.body);
+    }
+  }
 });
